f7077116657d287caf30a4c9785823a202d923c6
houssemhajri <houssemhajri10@gmail.com>
2025-05-22 01:38:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Autoriser CORS pour le frontend Angular
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
