7d1d304300b24bc9215cd48b975e477145a4ee86
houssemhajri <houssemhajri10@gmail.com>
2025-05-22 02:04:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Lire la variable PORT de Render
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 # Variables globales
 documents = []
 index = None
